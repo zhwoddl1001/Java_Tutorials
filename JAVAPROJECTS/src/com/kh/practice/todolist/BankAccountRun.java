097856f8afe943fe77bee4e4f53230712c45ae6b
zhwoddl1001 <zhwoddl0723@gmail.com>
2024-12-04 10:20:27 +0900

    java 실습

--- JAVAPROJECTS/src/com/kh/practice/todolist/BankAccountRun.java
+++ JAVAPROJECTS/src/com/kh/practice/todolist/BankAccountRun.java
@@ -1,4 +1,26 @@
 package com.kh.practice.todolist;
 
 public class BankAccountRun {
+    private System accountNumber;
+    private double balance;
+
+    public BankAccountRun(System accountNumber, double balance) {
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+    /*
+    * 
+    * deposit 입금 기능
+    * @param amount 입금할 금액
+    * 
+    * */ 
+    public void deposit(double amount) {
+        if (amount> 0){ // 입금할 금액이 0원보다 클 경우에만 입금 가능
+            balance += amount; // 현재 잔액에 입금 금액 더하기
+            System.out.println( amount + "원이 입금되었습니다 현재 잔액 : " + accountNumber+ "원");
+        }else {
+            System.out.println("올바른 금액을 입금해주세요.");
+        }
+    }
 }
